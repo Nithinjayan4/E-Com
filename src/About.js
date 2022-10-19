17eb294b7b2aa7567f04211baa7cdae889fc7a3e
Nithin S jayan <77042006+Nithinjayan4@users.noreply.github.com>
2022-10-19 21:56:33 +0530

    Context Api

--- src/About.js
+++ src/About.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import HeroSection from './Components/HeroSection'
-
+import { useProductContext } from "./Context/ProductContext";
 const About = () => {
-  const data={
-    name:"Thapa Ecom"
-  }
-  return<HeroSection myData={data}/>
-}
+  const { myName } = useProductContext();
+
+  const data = {
+    name: "Thapa Ecom",
+  };
+
+  return (
+    <>
+      {myName}
+      <HeroSection myData={data} />
+    </>
+  );
+};
+
 
 export default About
