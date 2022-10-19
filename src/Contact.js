11ea7892fc490d7232c777ee264052b6d8e71b56
Nithin S jayan <77042006+Nithinjayan4@users.noreply.github.com>
2022-10-19 20:05:40 +0530

    contact page form fixed

--- src/Contact.js
+++ src/Contact.js
@@ -4,23 +4,18 @@ const Contact = () => {
   const Wrapper = styled.section`
     padding: 9rem 0 5rem 0;
     text-align: center;
-
     .container {
       margin-top: 6rem;
-
       .contact-form {
         max-width: 50rem;
         margin: auto;
-
         .contact-inputs {
           display: flex;
           flex-direction: column;
           gap: 3rem;
-
           input[type="submit"] {
             cursor: pointer;
             transition: all 0.2s;
-
             &:hover {
               background-color: ${({ theme }) => theme.colors.white};
               border: 1px solid ${({ theme }) => theme.colors.btn};
@@ -33,7 +28,55 @@ const Contact = () => {
     }
   `;
 
-  return <Wrapper></Wrapper>;
+  return (
+    <Wrapper>
+      <h2 className="common-heading">Feel Free To Contact Us</h2>
+
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3939.1482990367876!2d76.5175854498763!3d9.141075789709431!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b061d9d232ab269%3A0xfbdd311d8dfba8cf!2sEriscoder%20Technologies!5e0!3m2!1sen!2sin!4v1666188387981!5m2!1sen!2sin"
+        width="100%"
+        height="400"
+        style={{ border: 0 }}
+        allowFullScreen=""
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"></iframe>
+
+      <div className="container">
+        <div className="contact-form">
+          <form
+            action="https://formspree.io/f/mjvzeebp"
+            method="POST"
+            className="contact-inputs">
+            <input
+              type="text"
+              placeholder="username"
+              name="username"
+              required
+              autoComplete="off"
+            />
+
+            <input
+              type="email"
+              name="Email"
+              placeholder="Email"
+              autoComplete="off"
+              required
+            />
+
+            <textarea
+              name="Message"
+              cols="30"
+              rows="10"
+              required
+              autoComplete="off"
+              placeholder="Enter you message"></textarea>
+
+            <input type="submit" value="send" />
+          </form>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Contact;
