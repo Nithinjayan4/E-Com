adecbc5e2112fddce0508a660a084c5cb02619fd
Nithin S jayan <77042006+Nithinjayan4@users.noreply.github.com>
2022-10-19 22:35:07 +0530

    statemanagement using useReducer

--- src/Context/ProductContext.js
+++ src/Context/ProductContext.js
@@ -1,12 +1,38 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect, useReducer } from "react";
+import axios from "axios";
+import reducer from "../Reducer/ProductReducer";
 
 const AppContext = createContext();
 
+const API = "https://api.pujakaitem.com/api/products";
+
+const initialState = {
+  isLoading: false,
+  isError: false,
+  products: [],
+  featureProducts: [],
+};
+
 const AppProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const getProducts = async (url) => {
+    dispatch({ type: "SET_LOADING" });
+    try {
+      const res = await axios.get(url);
+      const products = await res.data;
+      dispatch({ type: "SET_API_DATA", payload: products });
+    } catch (error) {
+      dispatch({ type: "API_ERROR" });
+    }
+  };
+
+  useEffect(() => {
+    getProducts(API);
+  }, []);
+
   return (
-    <AppContext.Provider value={{ myName: "Nithin S J" }}>
-      {children}
-    </AppContext.Provider>
+    <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
   );
 };
 
