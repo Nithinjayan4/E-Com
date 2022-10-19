26245f2dbc6eb605255d51e38452b8c416e3dc88
Nithin S jayan <77042006+Nithinjayan4@users.noreply.github.com>
2022-10-19 18:49:09 +0530

    trusted section & Service Completed

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,18 @@
 import React from "react";
 // import styled from "styled-components";
 import HeroSection from "./Components/HeroSection";
+import Services from "./Components/Services";
+import Trusted from "./Components/Trusted";
 
 const Home = () => {
 const data={
   name:"Thapa Store"
 }
 
-  return<HeroSection myData={data}/>
+  return <> <HeroSection myData={data}/>
+  <Services/>
+  <Trusted/>
+  </>
 };
 
 // const Wrapper = styled.section`
