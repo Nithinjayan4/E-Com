6e3675dfc48f13883652c775ff8a70cdd5dfe4b5
Nithin S jayan <77042006+Nithinjayan4@users.noreply.github.com>
2022-10-22 13:50:24 +0530

    Product Quantity

--- src/Components/CartAmountToggle.js
+++ src/Components/CartAmountToggle.js
@@ -2,19 +2,19 @@ import React from "react";
 import { FaMinus, FaPlus } from "react-icons/fa";
 
 const CartAmountToggle = ({ amount, setDecrease, setIncrease }) => {
-  return (
-    <div className="cart-button">
-      <div className="amount-toggle">
-        <button onClick={() => setDecrease()}>
-          <FaMinus />
-        </button>
-        <div className="amount-style">{amount}</div>
-        <button onClick={() => setIncrease()}>
-          <FaPlus />
-        </button>
+    return (
+      <div className="cart-button">
+        <div className="amount-toggle">
+          <button onClick={() => setDecrease()}>
+            <FaMinus />
+          </button>
+          <div className="amount-style">{amount}</div>
+          <button onClick={() => setIncrease()}>
+            <FaPlus />
+          </button>
+        </div>
       </div>
-    </div>
-  );
-};
-
-export default CartAmountToggle;
+    );
+  };
+  
+  export default CartAmountToggle;
